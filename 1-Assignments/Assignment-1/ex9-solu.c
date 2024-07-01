db54762d230bab8d5b459970eb32fd76e85b9b42
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-07-02 02:39:56 +0300

    Update ex9-solu.c

--- 1-Assignments/Assignment-1/ex9-solu.c
+++ 1-Assignments/Assignment-1/ex9-solu.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+int main(){
+	float grade;
+	printf("Enter your grade\n");
+	fflush(stdout);
+	scanf("%f",&grade);
+	if(grade>=85)
+	    printf("Excellent");
+	else if(grade>=75)
+	    printf("Very Good");
+	else if(grade>=65)
+	    printf("Good");
+	else if(grade>=50)
+	    printf("Pass");
+	else
+	    printf("Fail");
+  return 0;
+}
+
 
