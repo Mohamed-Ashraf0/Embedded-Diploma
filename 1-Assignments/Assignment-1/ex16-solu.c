27d309a539ac9146cb7c00b0ad44e750fc2f0c81
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-07-02 21:43:10 +0300

    Update ex16-solu.c

--- 1-Assignments/Assignment-1/ex16-solu.c
+++ 1-Assignments/Assignment-1/ex16-solu.c
@@ -1,1 +1,18 @@
-
+#include <stdio.h>
+int main()
+{
+   int num ,rev_num,temp,x=0;
+   printf("Enter the number\n");
+   fflush(stdout);
+   scanf("%d",&num);
+   while((pow(10,x))<num){
+	   x++;
+   }
+   for(int i=1;i<=x;i++){
+	   temp=num*pow(10,-(x-i));
+	   rev_num+=temp*pow(10,i-1);;
+	   num-=temp*pow(10,(x-i));
+   }
+   printf("reversed number =%d",rev_num);
+    return 0;
+}
