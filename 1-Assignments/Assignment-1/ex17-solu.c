fad1eaf57c4b99110df55d68b57e0d867dc51ef0
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-07-02 22:17:21 +0300

    Update ex17-solu.c

--- 1-Assignments/Assignment-1/ex17-solu.c
+++ 1-Assignments/Assignment-1/ex17-solu.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+int main()
+{
+   int num ,x=0;
+   printf("Enter the number\n");
+   fflush(stdout);
+   scanf("%d",&num);
+   while((pow(10,x))<num){
+	   x++;
+   }
+   printf("number of digits =%d",x);
+    return 0;
+}
+
 
