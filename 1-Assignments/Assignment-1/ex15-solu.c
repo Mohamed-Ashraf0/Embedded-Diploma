68a00b37ede01b3ff831592ec1b9800cc3bfb68f
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-07-02 04:19:27 +0300

    Update ex15-solu.c

--- 1-Assignments/Assignment-1/ex15-solu.c
+++ 1-Assignments/Assignment-1/ex15-solu.c
@@ -1,1 +1,15 @@
-
+#include <stdio.h>
+int main(){
+	int num, power, result=1;
+	    printf("Enter the number \n");
+	    fflush(stdout);
+	    scanf("%d", &num);
+	    printf("Enter the power \n");
+	    fflush(stdout);
+	    scanf("%d", &power);
+	    for(int i=1;i<=power;i++){
+	    	result*=num;
+	    }
+	    printf("%d power %d =%d",num, power, result);
+	    return 0;
+}
