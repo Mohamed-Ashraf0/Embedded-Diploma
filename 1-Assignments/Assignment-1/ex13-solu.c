aae1366805e8a1bf6c66d66ed4230b97ea92af17
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-07-02 04:05:16 +0300

    Update ex13-solu.c

--- 1-Assignments/Assignment-1/ex13-solu.c
+++ 1-Assignments/Assignment-1/ex13-solu.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+int main(){
+	int num ;
+    printf("Enter the  positive integer \n");
+	fflush(stdout);
+	scanf("%d",&num);
+    if(num <2){
+    	printf("%d is not a prime number",num);
+    	return 0;
+    }
+	for(int i=2;i<num;i++){
+		if(num%i==0){
+			printf("%d is not a prime number",num);
+			return 0;
+		}
+	}
+	printf("%d is  a prime number",num);
+
+  return 0;
+}
 
