0853468a2acd17218d8b679a8394307957f1a85e
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-07-02 03:32:59 +0300

    Update ex12-solu.c

--- 1-Assignments/Assignment-1/ex12-solu.c
+++ 1-Assignments/Assignment-1/ex12-solu.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+int main(){
+	int num , factorial=1;
+    printf("Enter the  positive integer \n");
+	fflush(stdout);
+	scanf("%d",&num);
+	for(int i=1;i<=num;i++){
+		factorial*=i;
+	}
+	printf("the factorial =%d",factorial);
+
+  return 0;
+}
+
 
