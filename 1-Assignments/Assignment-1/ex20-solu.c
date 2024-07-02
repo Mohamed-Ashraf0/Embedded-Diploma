91d261f3e958566a248f27147279c4bba1d996f2
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-07-03 01:02:07 +0300

    Update ex20-solu.c

--- 1-Assignments/Assignment-1/ex20-solu.c
+++ 1-Assignments/Assignment-1/ex20-solu.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 int main()
 {
-	int row , column ,temp_r1 ,temp_r2;
+	int row , column ,temp_c1 ,temp_c2;
 
 	printf("Enter number of rows: \n");
 	fflush(stdout);
     scanf("%d",&row);
     column=1+2*(row-1);
     for(int i=0 ;i<row;i++){
-    	temp_r1=(column/2)+1-i;
-    	temp_r2=(column/2)+1+i;
+    	temp_c1=(column/2)+1-i;
+    	temp_c2=(column/2)+1+i;
     	for(int j=1 ;j<=column;j++){
-    		if(j>=temp_r1 & j<=temp_r2)
+    		if(j>=temp_c1 & j<=temp_c2)
     			printf("*");
     		else
     			printf(" ");
