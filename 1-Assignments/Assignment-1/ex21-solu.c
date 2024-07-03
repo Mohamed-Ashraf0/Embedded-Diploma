810532d02a3e36fd31175bc3ee7779fa46563a51
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-07-03 23:25:11 +0300

    Update ex21-solu.c

--- 1-Assignments/Assignment-1/ex21-solu.c
+++ 1-Assignments/Assignment-1/ex21-solu.c
@@ -1,1 +1,19 @@
-
+#include <stdio.h>
+int main()
+{
+	int row ;
+	printf("Enter number of rows: \n");
+	fflush(stdout);
+    scanf("%d",&row);
+    for(int i=1 ;i<=row;i++){
+    	for(int j=1 ;j<=row;j++){
+    		if(j==i || j==row-i+1 ){
+    			printf(" *");
+    		}
+    		else
+    			printf(" ");
+    	}
+    	printf("\n");
+    }
+	return 0;
+}
