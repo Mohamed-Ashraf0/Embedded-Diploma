3cb7e038066a0ba256a3e873dc9e114f00a747aa
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-07-18 02:29:15 +0300

    Update ex8-solu.c

--- 1-Assignments/Assignment-2/ex8-solu.c
+++ 1-Assignments/Assignment-2/ex8-solu.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
 
+void prime_num(int x ,int y);
+
+int main(void)
+{
+	int num1,num2;
+	printf("Enter the first number ,second number\n");
+	fflush(stdout);
+	scanf("%d %d",&num1 ,&num2);
+	prime_num(num1,num2);
+
+}
+
+void prime_num(int x ,int y){
+	int flag;
+	for(int i=x;i<=y;i++){
+		flag=1;
+		for(int j=2;j<=i/2;j++){
+			if(i%j==0){
+				flag=0;
+				break;
+			}
+
+		}
+		if(flag && i>1){
+			printf("%d \n",i);
+		}
+	}
+}
