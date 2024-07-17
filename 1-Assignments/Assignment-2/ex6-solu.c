23cf9b29c4f490a3e06711f9b253785aa3bf9596
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-07-17 21:58:14 +0300

    Update ex6-solu.c

--- 1-Assignments/Assignment-2/ex6-solu.c
+++ 1-Assignments/Assignment-2/ex6-solu.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+
+void lower_to_upper(char x);
+
+int main(void)
+{
+	char x;
+	printf("Enter the character\n");
+	fflush(stdout);
+	scanf("%c",&x);
+	lower_to_upper(x);
+
+}
+
+void lower_to_upper(char x){
+	if(x>='a' && x<='z')
+	{
+		printf("Capital character is:- %c", (x-32));
+	}
+	else
+	{
+		printf("Invalid input");
+	}
+
+}
 
