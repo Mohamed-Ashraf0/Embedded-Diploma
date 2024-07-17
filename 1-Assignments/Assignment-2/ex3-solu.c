c89cf49f99c7adb8ca11fb5dc94406c385fff550
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-07-17 20:50:21 +0300

    Update ex3-solu.c

--- 1-Assignments/Assignment-2/ex3-solu.c
+++ 1-Assignments/Assignment-2/ex3-solu.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+void check_postive_negative(int x);
+
+int main(void){
+	int x;
+	printf("Enter the number:-\n");
+	fflush(stdout);
+	scanf("%d",&x);
+	check_postive_negative(x);
+    return 0;
+}
+
+void check_postive_negative(int x){
+	if(x>0)
+	{
+		printf("this number is positive");
+	}
+	else if(x<0)
+	{
+		printf("this number is negative");
+	}
+	else
+	{
+		printf("this number is zero");
+	}
+
+}
