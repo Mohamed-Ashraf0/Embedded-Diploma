258b96f56f6e9aa6dad8a57e6fb1a24c59880483
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-07-17 04:53:39 +0300

    Update ex1-solu.c

--- 1-Assignments/Assignment-2/ex1-solu.c
+++ 1-Assignments/Assignment-2/ex1-solu.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int cube(int x);
+
+int main(void){
+	int x;
+	printf("Enter the number:-\n");
+	fflush(stdout);
+	scanf("%d",&x);
+	printf("the cube of the number is:- %d",cube(x));
+}
+
+int cube(int x){
+	return x*x*x;
+}
