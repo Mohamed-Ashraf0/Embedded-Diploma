5f305edea1963f1f4a8c754635217baada72d248
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-07-17 21:49:36 +0300

    Update ex5-solu.c

--- 1-Assignments/Assignment-2/ex5-solu.c
+++ 1-Assignments/Assignment-2/ex5-solu.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+
+int even_odd(int x);
+
+int main(void)
+{
+	int num=0;
+	printf("Enter the number\n");
+	fflush(stdout);
+	scanf("%d",&num);
+	if(!even_odd(num))
+	{
+		printf("this number is even");
+	}
+	else
+	{
+	printf("this number is odd");
+	}
+}
+
+int even_odd(int x){
+	if(x%2==0)
+	{
+		return 0;
+	}
+	else
+	{
+		return 1;
+	}
+
+}
 
