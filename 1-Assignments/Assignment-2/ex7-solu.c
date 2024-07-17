ddcabf0e9145982471e52cbe2bf35400a942e8b7
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-07-17 22:10:42 +0300

    Update ex7-solu.c

--- 1-Assignments/Assignment-2/ex7-solu.c
+++ 1-Assignments/Assignment-2/ex7-solu.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+
+void first_mul_of_second(int x ,int y);
+
+int main(void)
+{
+	int num1,num2;
+	printf("Enter the first number ,second number\n");
+	fflush(stdout);
+	scanf("%d %d",&num1 ,&num2);
+	first_mul_of_second(num1,num2);
+
+}
+
+void first_mul_of_second(int x ,int y){
+	if(x%y==0)
+	{
+		printf("first is multiple of the second");
+	}
+	else
+	{
+		printf("first is not multiple of the second");
+	}
+
+}
 
