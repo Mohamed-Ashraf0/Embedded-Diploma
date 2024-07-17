923d1b30e0b12056892f801753f34718b3fc2554
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-07-18 02:37:02 +0300

    Update ex9-solu.c

--- 1-Assignments/Assignment-2/ex9-solu.c
+++ 1-Assignments/Assignment-2/ex9-solu.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+
+void swap(int *x ,int *y);
+
+int main(void)
+{
+	int num1,num2;
+	printf("Enter the first number ,second number\n");
+	fflush(stdout);
+	scanf("%d %d",&num1 ,&num2);
+	swap(&num1,&num2);
+    printf("first number= %d , second number=%d",num1,num2);
+}
+
+void swap(int *x ,int *y)
+{
+	int temp;
+	temp=*x;
+	*x=*y;
+	*y=temp;
+}
 
