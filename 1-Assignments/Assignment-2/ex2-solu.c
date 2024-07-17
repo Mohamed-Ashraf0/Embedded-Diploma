dd13ba5c211115efa904935e80d3e7c0561d40f2
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-07-17 20:10:30 +0300

    Update ex2-solu.c

--- 1-Assignments/Assignment-2/ex2-solu.c
+++ 1-Assignments/Assignment-2/ex2-solu.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+
+void check_alphabet(char x);
+
+int main(void){
+	char x;
+	printf("Enter the character:-\n");
+	fflush(stdout);
+	scanf("%c",&x);
+	check_alphabet(x);
+
+}
+
+void check_alphabet(char x){
+	if((x>='A' && x<='Z') || (x>='a' && x<='z')){
+		printf("this character is an alphabet.");
+	}
+	else
+		printf("this character is not an alphabet.");
+}
 
