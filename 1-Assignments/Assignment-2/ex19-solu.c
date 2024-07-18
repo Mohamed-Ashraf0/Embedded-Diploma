fb68655c7a5c25eb53717726ee0e11f3a8ffafb7
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-07-18 06:17:11 +0300

    Update ex19-solu.c

--- 1-Assignments/Assignment-2/ex19-solu.c
+++ 1-Assignments/Assignment-2/ex19-solu.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+
+int one_counter(unsigned short x );
+
+int main(void)
+{
+	unsigned short num;
+	printf("Enter the number :-\n");
+	fflush(stdout);
+	scanf("%hu",&num);
+	printf("the number of ones is:- %d",one_counter(num));
+}
+
+int one_counter(unsigned short x)
+{
+	int counter=0,temp=0;
+	while(x){
+		if(x%10==1){
+			temp++;
+		}
+		else
+		{
+			temp=0;
+		}
+		counter=counter<temp?temp :counter;
+		x/=10;
+	}
+	return counter;
+}
 
