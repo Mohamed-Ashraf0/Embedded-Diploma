a94947aa96b55ed46c738be040ab1fcebea34b9c
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-07-18 03:51:18 +0300

    Update ex11-solu.c

--- 1-Assignments/Assignment-2/ex11-solu.c
+++ 1-Assignments/Assignment-2/ex11-solu.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+#include <math.h>
+int pow_2(int x);
 
+int main(void)
+{
+	int num;
+	printf("Enter the number:-\n");
+	fflush(stdout);
+	scanf("%d",&num );
+	if(pow_2(num))
+	{
+		printf("the number is power of 2");
+	}
+	else
+	{
+		printf("the number is not power of 2");
+	}
+
+}
+
+int pow_2(int x)
+{
+	int i=0;
+	while(pow(2,i)<=x)
+	{
+		if(pow(2,i)==x){
+			return 1;
+		}
+		i++;
+	}
+	return 0;
+}
