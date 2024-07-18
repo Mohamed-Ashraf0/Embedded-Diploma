22ee28006f40c1b826d6f91d8dd1356898a0fe2b
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-07-18 04:21:04 +0300

    Update ex13-solu.c

--- 1-Assignments/Assignment-2/ex13-solu.c
+++ 1-Assignments/Assignment-2/ex13-solu.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+
+int float_adder(float x ,float y);
+
+int main(void)
+{
+	float num1,num2;
+	printf("Enter  number1 and number2:-\n");
+	fflush(stdout);
+	scanf("%f %f",&num1 ,&num2);
+	printf("the result is:- %d",float_adder(num1,num2));
+}
+
+int float_adder(float x ,float y)
+{
+
+	return (int)(x+y);
+}
 
