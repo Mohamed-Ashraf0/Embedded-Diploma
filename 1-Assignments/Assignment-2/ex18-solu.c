5d026a5bbe3d566418dbffcf48b90815084fefb5
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-07-18 06:06:54 +0300

    Update ex18-solu.c

--- 1-Assignments/Assignment-2/ex18-solu.c
+++ 1-Assignments/Assignment-2/ex18-solu.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+#include <math.h>
+int pow_3(int x);
 
+int main(void)
+{
+	int num;
+	printf("Enter the number:-\n");
+	fflush(stdout);
+	scanf("%d",&num );
+	if(!pow_3(num))
+	{
+		printf("the number is power of 3");
+	}
+	else
+	{
+		printf("the number is not power of 3");
+	}
+
+}
+
+int pow_3(int x)
+{
+	int i=0;
+	while(pow(3,i)<=x)
+	{
+		if(pow(3,i)==x){
+			return 0;
+		}
+		i++;
+	}
+	return 1;
+}
