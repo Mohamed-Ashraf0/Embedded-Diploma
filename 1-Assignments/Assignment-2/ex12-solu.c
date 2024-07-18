9fd5397f413f776997980e855f9cd009bda6e73d
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-07-18 04:13:01 +0300

    Update ex12-solu.c

--- 1-Assignments/Assignment-2/ex12-solu.c
+++ 1-Assignments/Assignment-2/ex12-solu.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
 
+int heater(int x);
+
+int main(void)
+{
+	int temp,time;
+	printf("Enter the temperature:-\n");
+	fflush(stdout);
+	scanf("%d",&temp );
+	time=heater(temp);
+	printf("the required heater activation time is:- %d",time);
+}
+
+int heater(int x )
+{
+	if(x>=0 && x<=30)
+	{
+		x=7;
+	}
+	else if(x>30 && x<=60)
+	{
+		x=5;
+	}
+	else if(x>60 && x<=90)
+	{
+		x=3;
+	}
+	else if(x>90 && x<=100)
+	{
+		x=1;
+	}
+	else
+	{
+		x=0;
+	}
+	return x;
+}
