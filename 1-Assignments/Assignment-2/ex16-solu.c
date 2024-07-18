9aefa6a46bbc46e49a8e005ebcadf1019193c0aa
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-07-18 05:39:43 +0300

    Update ex16-solu.c

--- 1-Assignments/Assignment-2/ex16-solu.c
+++ 1-Assignments/Assignment-2/ex16-solu.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+int one_counter(unsigned char x );
+
+int main(void)
+{
+	unsigned char num;
+	printf("Enter the number :-\n");
+	fflush(stdout);
+	scanf("%u",&num);
+	printf("the number of ones is:- %d",one_counter(num));
+}
+
+int one_counter(unsigned char x)
+{
+	int counter=0;
+	while(x){
+		if(x%10==1){
+			counter++;
+		}
+		x/=10;
+	}
+	return counter;
+}
