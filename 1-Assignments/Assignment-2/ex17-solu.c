06aa2871568ac8c41b44980b14a0d844370d91c3
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-07-18 06:00:15 +0300

    Update ex17-solu.c

--- 1-Assignments/Assignment-2/ex17-solu.c
+++ 1-Assignments/Assignment-2/ex17-solu.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
 
+int xor_max( int x ,int y);
+
+int main(void)
+{
+	int num1 , num2;
+	printf("Enter the number 1 and number 2 :-\n");
+	fflush(stdout);
+	scanf("%d %d",&num1,&num2);
+	printf("the number of ones is:- %d",xor_max(num1,num2));
+}
+
+int xor_max( int x ,int y)
+{
+	int max=0,result; //no negative output
+	for(int i=x;i<=y;i++){
+		for(int j=i;j<=y;j++){
+			result=i^j;
+			if(result>max){
+				max=result;
+			}
+		}
+	}
+	return max;
+}
