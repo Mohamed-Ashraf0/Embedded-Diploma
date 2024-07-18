1892f5ec8c5e0e5168be5c554b42819e429031ae
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-07-18 05:17:21 +0300

    Update ex14-solu.c

--- 1-Assignments/Assignment-2/ex14-solu.c
+++ 1-Assignments/Assignment-2/ex14-solu.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+int Fibonacci(int x );
+
+int main(void)
+{
+	int num;
+	printf("Enter the number :-\n");
+	fflush(stdout);
+	scanf("%d",&num);
+	printf("the result is:- %d",Fibonacci(num));
+}
+
+int Fibonacci(int x)
+{
+	if(x<2)
+	{
+		return x;
+	}
+	else
+	{
+		return Fibonacci(x-1) + Fibonacci(x-2);
+	}
+
+}
