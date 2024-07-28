dd5051a61720b6c2ddc6c98eaa1a670616942d4e
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-07-29 00:22:30 +0300

    Update ex10-solu.c

--- 1-Assignments/Assignment-3/ex10-solu.c
+++ 1-Assignments/Assignment-3/ex10-solu.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
 
+
+void upper_to_lower(char *str);
+
+int main(void)
+{
+	char str[21];
+	printf("enter string of max 20 char :-\n");
+	fflush(stdout);
+	scanf("%s",str);
+	upper_to_lower(str);
+	printf("lower case string is:- %s",str);
+}
+
+void upper_to_lower(char *str)
+{
+	int i=0;
+	while(str[i]!='\0')
+	{
+		if(str[i]>='A' && str[i]<='Z')
+		{
+			str[i]+=32;
+		}
+		i++;
+	}
+}
