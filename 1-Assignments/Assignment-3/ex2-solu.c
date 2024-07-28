c626aba2fc7265b2b241a6d3a23ecb1e92cb3be6
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-07-28 21:11:56 +0300

    Update ex2-solu.c

--- 1-Assignments/Assignment-3/ex2-solu.c
+++ 1-Assignments/Assignment-3/ex2-solu.c
@@ -1,1 +1,46 @@
+#include <stdio.h>
+
+#define TRUE 1
+#define FALSE 0
+
+int distinct_string(char *str);
+
+int main(void)
+{
+	char str[21];
+	int result;
+	printf("enter string of max 20 char :-\n");
+	fflush(stdout);
+	scanf("%s",str);
+	result=distinct_string(str);
+	if(result)
+	{
+	printf("String is distinct");
+	}
+	else
+	{
+		printf("String is not distinct");
+	}
+}
+
+int distinct_string(char *str)
+{
+	int result =TRUE;
+	int i=0,j=0;
+	while(result && str[i]!='\0')
+	{
+		j=i+1;
+		while(str[j]!='\0')
+		{
+			if(str[i]==str[j])
+			{
+				result=FALSE;
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	return result;
+}
 
