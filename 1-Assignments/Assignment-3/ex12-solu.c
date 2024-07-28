49e7a72d47ed6da015269ab85e869a04a51fe237
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-07-29 00:43:47 +0300

    Update ex12-solu.c

--- 1-Assignments/Assignment-3/ex12-solu.c
+++ 1-Assignments/Assignment-3/ex12-solu.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
 
+
+int str_length(char *str );
+
+int main(void)
+{
+	char str[21] ;
+	int length;
+	printf("enter string of max 20 char :-\n");
+	fflush(stdout);
+	gets(str);
+	length=str_length(str);
+	printf("string length is:- %d",length);
+}
+
+int str_length(char *str)
+{
+	int i=0;
+	while(str[i]!='\0')
+	{
+
+		i++;
+	}
+	return i;
+}
