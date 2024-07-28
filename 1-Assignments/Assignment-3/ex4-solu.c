4767fa368d17c458415f59df292984fce174fa2b
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-07-28 22:46:04 +0300

    Update ex4-solu.c

--- 1-Assignments/Assignment-3/ex4-solu.c
+++ 1-Assignments/Assignment-3/ex4-solu.c
@@ -1,1 +1,43 @@
+#include <stdio.h>
+
+#define SIZE 10
+
+void Selectionsort(int *arr ,int size);
+
+int main(void)
+{
+	int arr[SIZE];
+	printf("enter 10 numbers :-\n");
+	fflush(stdout);
+	for(int i=0;i<SIZE;i++)
+	{
+		scanf("%d",(arr+i));
+	}
+	Selectionsort(arr,SIZE);
+	printf("array after ascending order :-\n");
+	fflush(stdout);
+	for(int i=0;i<SIZE;i++)
+	{
+		printf("%d ",arr[i]);
+	}
+
+}
+
+void Selectionsort(int *arr ,int size)
+{
+	int min_index;
+	for(int i=0 ;i<size-1;i++)
+	{
+		min_index=i;
+		for(int j=i+1;j<size;j++)
+		{
+			if(arr[min_index]>arr[j])
+			{
+				arr[j]=arr[j]+arr[min_index];
+				arr[min_index]=arr[j]-arr[min_index];
+				arr[j]=arr[j]-arr[min_index];
+			}
+		}
+	}
+}
 
