37fb83289cef8ed42ff9c0e7ad74e0ca772d8a67
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-07-29 00:02:34 +0300

    Update ex9-loop-solu.c

--- 1-Assignments/Assignment-3/ex9-loop-solu.c
+++ 1-Assignments/Assignment-3/ex9-loop-solu.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+
+#define SIZE 100
+
+void fibonacci (int *arr,int term);
+
+int main(void)
+{
+	int arr[SIZE], n;
+	printf("enter the index of fibonacci number:-\n");
+	fflush(stdout);
+	scanf("%d",&n);
+	fibonacci(arr,n);
+	printf("the number is:- %d",arr[n-1]);
+}
+
+void fibonacci (int *arr,int term)
+{
+	arr[0]=1;
+	arr[1]=1;
+	for(int i=2;i<term;i++)
+	{
+		arr[i]=arr[i-1]+arr[i-2];
+	}
+}
 
