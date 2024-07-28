01f873cda798814a10336b680577e4c7797be4ca
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-07-28 13:46:23 +0300

    Update ex1-solu.c

--- 1-Assignments/Assignment-3/ex1-solu.c
+++ 1-Assignments/Assignment-3/ex1-solu.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#define SIZE 5
+
+int countoccurrences(int *arr,int size);
+
+int main(void)
+{
+	int arr[SIZE];
+	int result;
+	printf("enter 5 array elements:-\n");
+	fflush(stdout);
+	for(int i=0;i<SIZE;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	result=countoccurrences(arr,SIZE);
+	printf("sum of array is:- %d",result);
+}
+
+int countoccurrences(int *arr,int size)
+{
+	int sum=0;
+	for(int i=0;i<size;i++)
+	{
+		sum+=arr[i];
+	}
+	return sum;
+}
 
