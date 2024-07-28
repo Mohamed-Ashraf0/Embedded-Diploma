28a314da4c69bb67927bd2dde6456682f5bc4280
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-07-29 01:04:49 +0300

    Update ex14-solu.c

--- 1-Assignments/Assignment-3/ex14-solu.c
+++ 1-Assignments/Assignment-3/ex14-solu.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include<string.h>
+
+void reverse_string(char *str);
+
+int main(void)
+{
+	char str[21];
+	printf("enter string of max 20 char :-\n");
+	fflush(stdout);
+	scanf("%s",str);
+	reverse_string(str);
+	printf("reversed string is:- %s",str);
+}
+
+void reverse_string(char *str)
+{
+	int size=strlen(str);
+	char temp;
+	for(int i=0;i<size/2;i++)
+	{
+		temp=str[i];
+		str[i]=str[size-1-i];
+		str[size-1-i]=temp;
+	}
+}
 
