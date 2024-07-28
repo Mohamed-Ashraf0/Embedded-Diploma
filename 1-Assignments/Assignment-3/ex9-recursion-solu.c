1b496fe1ddcda3171c9764ac68d6ca117110be52
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-07-29 00:16:09 +0300

    Update ex9-recursion-solu.c

--- 1-Assignments/Assignment-3/ex9-recursion-solu.c
+++ 1-Assignments/Assignment-3/ex9-recursion-solu.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+
+
+
+int fibonacci (int n);
+
+int main(void)
+{
+	int  n;
+	printf("enter the index of fibonacci number:-\n");
+	fflush(stdout);
+	scanf("%d",&n);
+	printf("the number is:- %d",fibonacci(n));
+}
+
+int fibonacci (int n)
+{
+	if(n==0 || n==1)
+	{
+		return n;
+	}
+	else
+		return fibonacci(n-1)+fibonacci(n-2);
+}
 
