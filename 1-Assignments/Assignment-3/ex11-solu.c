9557f07a35bb766c591039b97e062670d551a2a9
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-07-29 00:34:42 +0300

    Update ex11-solu.c

--- 1-Assignments/Assignment-3/ex11-solu.c
+++ 1-Assignments/Assignment-3/ex11-solu.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
 
+
+int char_freq(char *str ,char target);
+
+int main(void)
+{
+	char str[21] , target;
+	int frequency;
+	printf("enter string of max 20 char :-\n");
+	fflush(stdout);
+	scanf("%s",str);
+	printf("enter the target :-\n");
+	fflush(stdout);
+	scanf(" %c",&target);
+	frequency=char_freq(str,target);
+	printf("char '%c' is repeated %d times",target,frequency);
+}
+
+int char_freq(char *str,char target)
+{
+	int i=0,freq=0;
+	while(str[i]!='\0')
+	{
+		if(str[i]==target)
+		{
+			freq++;
+		}
+		i++;
+	}
+	return freq;
+}
