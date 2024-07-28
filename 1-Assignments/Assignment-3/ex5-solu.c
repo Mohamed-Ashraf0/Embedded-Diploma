e0d2b64f9ad0a17d38e92f2a8390b00c70e9ce60
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-07-28 23:29:42 +0300

    Update ex5-solu.c

--- 1-Assignments/Assignment-3/ex5-solu.c
+++ 1-Assignments/Assignment-3/ex5-solu.c
@@ -1,1 +1,43 @@
+#include <stdio.h>
 
+#define SIZE 10
+
+int LinearSearch (int *arr ,int size,int target);
+
+int main(void)
+{
+	int arr[SIZE],index,target;
+	printf("enter 10  numbers :-\n");
+	fflush(stdout);
+	for(int i=0;i<SIZE;i++)
+	{
+		scanf("%d",(arr+i));
+	}
+	printf("enter the target :-\n");
+	fflush(stdout);
+	scanf("%d",&target);
+	index=LinearSearch(arr,SIZE,target);
+	if(index==-1)
+	{
+		printf("element not found");
+	}
+	else
+	{
+		printf("element index is:- %d",index);
+	}
+
+}
+
+int LinearSearch(int *arr ,int size ,int target)
+{
+	int index=-1;
+	for(int i=0;i<size;i++)
+	{
+		if(arr[i]==target)
+		{
+			index=i;
+			break;
+		}
+	}
+	return index;
+}
