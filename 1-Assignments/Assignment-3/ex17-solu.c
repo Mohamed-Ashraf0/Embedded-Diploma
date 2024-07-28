0f93b498b7e2e9e8a677576f659819e741f9f796
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-07-29 01:39:26 +0300

    Update ex17-solu.c

--- 1-Assignments/Assignment-3/ex17-solu.c
+++ 1-Assignments/Assignment-3/ex17-solu.c
@@ -1,1 +1,45 @@
+#include <stdio.h>
+
+#define SIZE 5
+
+void swap_array(int *arr1,int size1,int *arr2,int size2);
+
+int main(void)
+{
+	int arr1[SIZE],arr2[SIZE],i;
+	printf("enter 5 numbers :-\n");
+	fflush(stdout);
+	for(i=0;i<SIZE;i++)
+	{
+		scanf("%d",(arr1+i));
+	}
+	printf("enter 5 numbers :-\n");
+	fflush(stdout);
+	for(i=0;i<SIZE;i++)
+	{
+		scanf("%d",(arr2+i));
+	}
+	swap_array(arr1,SIZE,arr2,SIZE);
+	printf("first array after is:-");
+	for(i=0;i<SIZE;i++)
+	{
+		printf("%d ",arr1[i]);
+	}
+	printf("\nsecond array after is:-");
+	for(i=0;i<SIZE;i++)
+	{
+		printf("%d ",arr2[i]);
+	}
+}
+
+void swap_array(int *arr1,int size1,int *arr2,int size2)
+{
+	int temp;
+	for(int i=0;i<size2;i++)
+	{
+		temp=arr1[i];
+		arr1[i]=arr2[i];
+		arr2[i]=temp;
+	}
+}
 
