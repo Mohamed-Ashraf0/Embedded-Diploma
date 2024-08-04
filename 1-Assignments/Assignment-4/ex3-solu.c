c60960732e74b6a4d67b048f25b58d38bae566e4
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-08-04 21:43:51 +0300

    Update ex3-solu.c

--- 1-Assignments/Assignment-4/ex3-solu.c
+++ 1-Assignments/Assignment-4/ex3-solu.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+
+#define SIZE 10
+
+int main(void)
+{
+	int i;
+	int arr[SIZE]={1,2,3,4,5,6,7,8,9,10};
+	int *ptr=arr;
+	for(i=SIZE-1;i>=0;i--)
+	{
+		printf("%d ",*(ptr+i));
+	}
+
+}
 
