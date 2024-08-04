58d9f94bf96ea1e0521cd56e1ee4ee4aac152c81
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-08-05 01:27:45 +0300

    Update ex1-solu.c

--- 1-Assignments/Assignment-5/ex1-solu.c
+++ 1-Assignments/Assignment-5/ex1-solu.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+
+
+struct student
+{
+	char str[20];
+	int roll;
+	int marks;
+};
+
+int main(void)
+{
+	struct student mohamed={"mohamed",3,97};
+	printf("student name:%s\nroll:%d\nmarks:%d",mohamed.str,mohamed.roll,mohamed.marks);
+}
+
 
