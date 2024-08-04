751d607468ff8d7069067c6fa56e32c2c82d5981
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-08-04 23:14:49 +0300

    Update ex4-solu.c

--- 1-Assignments/Assignment-4/ex4-solu.c
+++ 1-Assignments/Assignment-4/ex4-solu.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+
+#define SIZE 10
+
+int smallest_element (int *arr,int size);
+
+int main(void)
+{
+	int result;
+	int arr[SIZE]={14,2,6,3,9,10,8,1,7,12};
+	result=smallest_element(arr,SIZE);
+    printf("smallest value is:- %d",result);
+
+}
+
+int smallest_element (int *arr,int size)
+{
+	int i,min=*arr;
+	for(i=1;i<size;i++)
+	{
+		if(*(arr+i)<min)
+		{
+			min=*(arr+i);
+		}
+	}
+	return min;
+
+}
 
