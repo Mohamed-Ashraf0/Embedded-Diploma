e46f92ad5f4866093949c67ee80ae8ddf9316c18
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-08-04 20:38:11 +0300

    Update ex1-solu.c

--- 1-Assignments/Assignment-4/ex1-solu.c
+++ 1-Assignments/Assignment-4/ex1-solu.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+
+#define SIZE 10
+
+int main(void)
+{
+	int i,sum=0;
+int arr[SIZE]={2,4,6,8,1,3,5,7,9,10};
+int *ptr=arr;
+for(i=0;i<SIZE;i++)
+{
+	sum+=*(ptr+i);
+}
+printf("sum of the array elemrnts is:- %d",sum);
+}
 
