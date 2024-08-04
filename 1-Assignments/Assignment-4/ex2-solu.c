d1ab47d40f967a5df637af19bd8e42574330efa4
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-08-04 21:28:06 +0300

    Update ex2-solu.c

--- 1-Assignments/Assignment-4/ex2-solu.c
+++ 1-Assignments/Assignment-4/ex2-solu.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+
+#define SIZE 10
+
+int main(void)
+{
+	int size=0;
+char str[SIZE]="HELLO";
+char *ptr=str;
+while(*ptr!='\0')
+{
+	size++;
+	ptr++;
+}
+printf("size of the string is:- %d",size);
+}
 
