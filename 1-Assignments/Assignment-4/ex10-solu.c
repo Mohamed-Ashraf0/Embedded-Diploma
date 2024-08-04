524333191956ee07f07427564f493e115bd09ae9
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-08-05 00:16:08 +0300

    Update ex10-solu.c

--- 1-Assignments/Assignment-4/ex10-solu.c
+++ 1-Assignments/Assignment-4/ex10-solu.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include<string.h>
+
+void pointer_swap (int **ptr1,int **ptr2);
+
+int main(void)
+{
+int num1=5,num2=10;
+int *ptr1=&num1 ,*ptr2=&num2;
+pointer_swap(&ptr1,&ptr2);
+printf("pointer 1 points to:- %d\npointer 2 points to:- %d",*ptr1,*ptr2);
+}
+
+void pointer_swap (int **ptr1,int **ptr2)
+{
+	int *temp=*ptr1;
+	*ptr1=*ptr2;
+	*ptr2=temp;
+}
 
