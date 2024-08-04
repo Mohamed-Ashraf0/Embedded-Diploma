ecdaaee2c6d1ae4b3d336701d9971a0d99b28ef4
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-08-05 00:05:02 +0300

    Update ex9-solu.c

--- 1-Assignments/Assignment-4/ex9-solu.c
+++ 1-Assignments/Assignment-4/ex9-solu.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include<string.h>
+
+int main(void)
+{
+	char str1 [20]="bat",str2[4];
+	int size =strlen(str1);
+	if(size >= 3)
+	{
+		str2[0]=str1[size-1];
+		str2[1]=' ';
+		str2[2]=str1[size-2];
+		str2[3]='\0';
+		printf("new string is:- %s",str2);
+	}
+	else
+	{
+		printf("enter string greater than or equal 3 characters");
+	}
+}
 
