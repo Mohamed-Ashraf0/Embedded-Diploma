062ccefea33e8926c4129229f6c399af24a1ae65
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-08-05 01:39:56 +0300

    Update ex2-solu.c

--- 1-Assignments/Assignment-5/ex2-solu.c
+++ 1-Assignments/Assignment-5/ex2-solu.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+
+struct distance
+{
+	float inch;
+	float feet;
+}d1,d2,sum;
+
+int main(void)
+{
+	printf("enter first distance in inch:- ");
+	fflush(stdout);
+	scanf("%f",&d1.inch);
+	printf("enter first distance in feet:- ");
+	fflush(stdout);
+	scanf("%f",&d1.feet);
+	printf("enter second distance in inch:- ");
+	fflush(stdout);
+	scanf("%f",&d2.inch);
+	printf("enter second distance in feet:- ");
+	fflush(stdout);
+	scanf("%f",&d2.feet);
+	sum.inch=d1.inch+d2.inch;
+	sum.feet=d1.feet+d2.feet;
+	printf("sum of two distance in inch is:- %.1f\n",sum.inch);
+	printf("sum of two distance in feet is:- %.1f",sum.feet);
+}
