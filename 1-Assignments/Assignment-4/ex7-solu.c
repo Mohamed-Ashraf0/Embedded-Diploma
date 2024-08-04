b1a9467793d0088594dec1e989084ddb7ec10482
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-08-04 23:48:02 +0300

    Update ex7-solu.c

--- 1-Assignments/Assignment-4/ex7-solu.c
+++ 1-Assignments/Assignment-4/ex7-solu.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+
+#define SIZE 10
+
+void swap (int *ptr1,int *ptr2);
+
+int main(void)
+{
+	int num1 =5,num2=8;
+	void (*ptr_func)(int*,int*)=swap;
+	(*ptr_func)(&num1,&num2);
+	printf("after swap number 2 = %d",num2);
+
+
+}
+
+void swap (int *ptr1,int *ptr2)
+{
+	*ptr1 = *ptr1 ^ *ptr2;
+	*ptr2 = *ptr1 ^ *ptr2;
+	*ptr1 = *ptr1 ^ *ptr2;
+}
 
