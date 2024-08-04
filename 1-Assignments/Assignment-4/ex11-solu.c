a9e35d9aaa9c427bb4e7fcf2fc7a8dcf3b4c1399
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-08-05 00:38:51 +0300

    Update ex11-solu.c

--- 1-Assignments/Assignment-4/ex11-solu.c
+++ 1-Assignments/Assignment-4/ex11-solu.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include<string.h>
+
+void bits_swap (  int *ptr);
+
+int main(void)
+{
+  int num=0xa4c3f872;
+bits_swap(&num);
+printf("number after swap is:- %x",num);
+}
+
+void bits_swap (int *ptr)
+{
+	 short *ptr1=(short*)ptr;
+	 short temp;
+	temp=ptr1[1];
+	ptr1[1]=ptr1[0];
+	ptr1[0]=temp;
+}
 
