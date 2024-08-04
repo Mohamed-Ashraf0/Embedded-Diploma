0f66b937a1724d8c21b921e69ec7ee0e4cc67c8f
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-08-05 01:44:46 +0300

    Update ex3-solu.c

--- 1-Assignments/Assignment-5/ex3-solu.c
+++ 1-Assignments/Assignment-5/ex3-solu.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+
+struct complex
+{
+	float real;
+	float imag;
+}num1,num2,sum;
+
+int main(void)
+{
+	printf("enter first real in inch:- ");
+	fflush(stdout);
+	scanf("%f",&num1.real);
+	printf("enter first imaginary in feet:- ");
+	fflush(stdout);
+	scanf("%f",&num1.imag);
+	printf("enter second real in inch:- ");
+	fflush(stdout);
+	scanf("%f",&num2.real);
+	printf("enter second imaginary in feet:- ");
+	fflush(stdout);
+	scanf("%f",&num2.imag);
+	sum.real=num1.real+num2.real;
+	sum.imag=num1.imag+num2.imag;
+	printf("sum of two nums is:-  inch is:- %.1f +%.1fi\n",sum.real,sum.imag);
+
+}
