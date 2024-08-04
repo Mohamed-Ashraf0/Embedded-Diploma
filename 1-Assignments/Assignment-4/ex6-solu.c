3e5f568246e5415518814bbcff279c440fe28073
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-08-04 23:24:32 +0300

    Update ex6-solu.c

--- 1-Assignments/Assignment-4/ex6-solu.c
+++ 1-Assignments/Assignment-4/ex6-solu.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
 
+#define SIZE 10
+
+int largest_element (int *arr,int size);
+
+int main(void)
+{
+	int result,i;
+	int arr[SIZE]={14,2,6,3,9,10,8,1,7,12};
+	result=largest_element(arr,SIZE);
+	printf("array elements is:- ");
+	for(i=0;i<SIZE;i++)
+	{
+		printf("%d ",arr[i]);
+	}
+	printf("\nlargest value is:- %d",result);
+
+}
+
+int largest_element (int *arr,int size)
+{
+	int i,max=*arr;
+	for(i=1;i<size;i++)
+	{
+		if(*(arr+i)>max)
+		{
+			max=*(arr+i);
+		}
+	}
+	return max;
+
+}
