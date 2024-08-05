075be8be185098101b32d2631f0606f71b877a00
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-08-05 22:25:03 +0300

    Update ex6-solu.c

--- 1-Assignments/Assignment-5/ex6-solu.c
+++ 1-Assignments/Assignment-5/ex6-solu.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef union family_name
+{
+	char  first_name[30];
+	char  last_name[30];
+}family_name;
+
+int main(void)
+{
+	family_name x;
+	strcpy(x.first_name,"Mohamed");
+	printf("last name :- %s\n",x.last_name);
+	printf("size of union is:- %d",sizeof(family_name));
+}
+
+
 
