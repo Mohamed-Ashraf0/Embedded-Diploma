ef62e83d33b4c1557bc0d3bc7308a0417c3a1df9
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-08-05 22:56:35 +0300

    Update ex7-solu.c

--- 1-Assignments/Assignment-5/ex7-solu.c
+++ 1-Assignments/Assignment-5/ex7-solu.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+enum fan_level{
+	Level1,
+	Level2,
+	Level3,
+};
+int main(void)
+{
+	enum fan_level fan1=Level2;
+	switch (fan1)
+	{
+	case Level1:
+		printf("fan is level 1");
+		break;
+	case Level2:
+		printf("fan is level 2");
+		break;
+	case Level3:
+		printf("fan is level 3");
+		break;
+	}
+}
 
