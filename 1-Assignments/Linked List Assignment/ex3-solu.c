ab87d0924df0466acf655517c774eda9fd9b1c56
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-08-09 02:34:42 +0300

    Update ex3-solu.c

--- 1-Assignments/Linked List Assignment/ex3-solu.c
+++ 1-Assignments/Linked List Assignment/ex3-solu.c
@@ -73,64 +73,6 @@ void print_list (void)
 		ptr=ptr->next;
 	}
 }
-void insert_element(int pos ,int data)
-{
-	if(head==NULL)
-	{
-		printf("list is empty");
-		return;
-	}
-	if(pos>size_of_list())
-	{
-		printf("index not found");
-		return;
-	}
-	int i;
-	struct node *ptr_prev=head;
-	struct node *ptr_current=ptr_prev->next;
-	struct node *link=(struct node*)malloc(sizeof(struct node));
-	link->data=data;
-	if(pos==0)
-	{
-		link->next=head;
-		head=link;
-		return;
-	}
-	//to move current to passed position
-	for(i=1;i<pos;i++)
-	{
-		ptr_prev=ptr_prev->next;
-		ptr_current=ptr_current->next;
-	}
-	ptr_prev->next=link;
-	link->next=ptr_current;
-}
-int find_middle_node (void)
-{
-	if(head==NULL)
-	{
-		printf("empty list");
-		return 0;
-	}
-	if(head->next == NULL)
-	{
-		return head->data;;
-	}
-	struct node *ptr1=head;
-	struct node *ptr2=head;
-	int x=0;
-	while(ptr1!=NULL)
-	{
-		ptr1=ptr1->next;
-		x++;
-		if(x==2)
-		{
-			ptr2=ptr2->next;
-			x=0;
-		}
-	}
-	return ptr2->data;
-}
 int find_fifth_node_from_end (void)
 {
 	if(head==NULL)
