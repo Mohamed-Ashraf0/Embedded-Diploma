44d0b1b237c6a379627e8db9dd3a64aee38f0694
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-09-07 08:02:52 +0300

    Update student.h

--- 2-projects/c_project/student.h
+++ 2-projects/c_project/student.h
@@ -1,1 +1,46 @@
+/*
+ * student.h
+ *
+ *  Created on: Aug 9, 2024
+ *      Author: Moham
+ */
 
+#ifndef STUDENT_H_
+#define STUDENT_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct student
+{
+	int id; //student id
+	char name[50]; //student name
+	int age; //student age
+	float gpa; //student GPA
+};
+//linked list
+struct node
+{
+	struct student data;
+	struct node *next;
+};
+
+
+void addStudent(const struct student *const ptr);
+
+void displayStudents(void);
+
+void searchStudentByID(int id);
+
+void updateStudent(int id);
+
+float calculateAverageGPA(void);
+
+void searchHighestGPA(void);
+
+void deleteStudent(int id);
+
+void display_menu(void);
+
+
+#endif /* STUDENT_H_ */
